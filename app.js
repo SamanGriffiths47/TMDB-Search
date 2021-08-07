3340a121910e6ca4c72db879306c18fa861cb0f4
Sam'an <samangriffiths47@gmail.com>
2021-08-07 00:58:44 -0500

    nice

--- app.js
+++ app.js
@@ -10,25 +10,46 @@ button.addEventListener('click', async () => {
   const input = `${DOMAIN}/search/movie?api_key=${API_KEY}&query=${userSearch}`
   let response = await axios.get(input)
   let API_RESULTS = response.data.results
+  let h2 = document.querySelector(`h2`)
   document.body.style.height = `auto`
+  h2.innerText = ``
+  h2.innerText = `(Hover Over Image)`
   document.querySelector(`section`).innerHTML = ``
   API_RESULTS.forEach((result) => {
     let title = result.title
     let lang = result.original_language
     let date = result.release_date
     let pic = result.poster_path
-    let img = document.createElement(`img`)
-    img.src = `${IMAGE_BASE_PATH + pic}`
-    img.alt = `Movie Poster`
     let section = document.querySelector(`section`)
     let list = document.createElement(`li`)
-    list.innerHTML = `<b>Title:</b> ${title}</br> <b>Date:</b> ${date}</br> <b>Original Language:</b> ${lang}`
+    let img = document.createElement(`img`)
     let div = document.createElement(`div`)
-    section.appendChild(div).appendChild(list)
+    img.src = `${IMAGE_BASE_PATH + pic}`
+    img.alt = `Movie Poster`
+    list.innerHTML = `<b>Title:</b> ${title}</br> <b>Date:</b> ${date}</br> <b>Original Language:</b> ${lang}`
     section.appendChild(div).appendChild(img)
+    section.appendChild(div).appendChild(list)
   })
+  for (let i = 0; i < document.querySelectorAll(`div`).length; i++) {
+    document.querySelectorAll(`div`)[i].className = 'movie'
+  }
+  for (let i = 0; i < document.querySelectorAll(`li`).length; i++) {
+    document.querySelectorAll(`li`)[i].className = 'description'
+  }
+  let poster = document.getElementsByTagName(`div`)
+  for (let i = 0; i < poster.length; i++) {
+    poster[i].addEventListener('mouseover', function (event) {
+      event.currentTarget.childNodes[1].className = `description-posterli`
+      event.currentTarget.className = `movie-posterdiv`
+    })
+  }
+  for (let i = 0; i < poster.length; i++) {
+    poster[i].addEventListener('mouseout', function (event) {
+      event.currentTarget.childNodes[1].className = `description`
+      event.currentTarget.className = `movie`
+    })
+  }
 })
-
 query.addEventListener('keyup', function (event) {
   if (event.keyCode === 13) {
     event.preventDefault()
